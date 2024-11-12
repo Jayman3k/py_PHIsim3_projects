e73a2ddf5a29efbf20e391861024f38a6140cb69
Joris Borms <j.j.j.borms@tue.nl>
2024-11-12 13:36:58 +0100

    more updates to animation code

--- tools/PHIsim_animate.py
+++ tools/PHIsim_animate.py
@@ -4,9 +4,11 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from contextlib import chdir
 from tqdm import tqdm
+from dataclasses import dataclass
 
 from tools.PHIsim_sim_params import PHIsim_SimulationParams
-from tools.fmt_utils import fmt_eng
+from tools.PHIsim_dispatcher import PHIsim_Result
+from tools.fmt_utils import fmt_eng, SI_prefix_scaling
 
 # we print a warning for large videos, and error/abort for unreasonably large videos
 # these limits can be overriden if you *really* want.
@@ -15,6 +17,272 @@ DEFAULT_SANITY_CHECK_LIMITS = dict(
     warn =  2000,
     error = 20000
 )
+
+"""
+Some of the code below is based on a script provided by Erwin Bente (e.a.j.m.bente@tue.nl), 
+which was packaged with PHIsim (https://sites.google.com/tue.nl/phisim/home),
+and originally adapted the code from Jake Vanderplas, see below:
+------------------------------
+Matplotlib Animation Example
+
+author: Jake Vanderplas
+email: vanderplas@astro.washington.edu
+website: http://jakevdp.github.com
+license: BSD
+Please feel free to use and modify this, but keep the above information. Thanks!
+http://jakevdp.github.io/blog/2012/08/18/matplotlib-animation-tutorial/
+"""
+
+def animate_video_with_IO(work_folder, output_filename, 
+                          sim_params: PHIsim_SimulationParams,
+                          time_window: tuple[float, float], 
+                          **kwargs):
+    """Animate a time slice of the simulation run by PHIsim. 
+    
+    Currently, this is designed to animate a 3-panel video of the laser, with
+    LR input on the left panel, LR output on the right panel, and the laser in the center.
+
+    If you have a different use-case, and you're not me (is anyone using this code?)
+    feel free to experiment with the code or send me a question.
+
+    See the constructor for additional optional arguments passed via kwargs. 
+    """
+    _VideoAnimator(work_folder, output_filename, sim_params, time_window, **kwargs).animate()
+
+
+class _VideoAnimator:
+    """Helper class to animate a time slice of the simulation run by PHIsim. """	
+
+    @dataclass
+    class VideoData:
+        LRp : np.ndarray
+        RLp : np.ndarray
+        car : np.ndarray
+
+    def __init__(self, work_folder, output_filename, 
+                 sim_params: PHIsim_SimulationParams,
+                 time_window: tuple[float, float], 
+                 **kwargs):
+        
+        self.work_folder = work_folder
+        self.output_filename = output_filename
+        self.sim_params = sim_params
+        self.time_window = time_window
+
+        self.frame_range = time_window_to_video_frames(time_window, sim_params)
+        self.frame_delta_t = sim_params.simulation_time_step() * sim_params.video_N
+        self.video_start_t = sim_params.video_start * sim_params.simulation_time_step()
+
+        self.laser_pigtail = kwargs.pop("laser_pigtail", None) # in meter
+
+        # I'm assuming that mW, μm and ns will be a reasonable x/y-scale for most PHIsim simulations
+        # but this can be overriden via kwargs
+        self.segment_unit    = kwargs.pop("segment_unit", "μm")
+        self.time_unit       = kwargs.pop("segment_unit", "ns")
+        self.power_unit      = kwargs.pop("power_unit",   "mW")
+        self.segment_scaling = self._parse_scaling(self.segment_unit, "m") 
+        self.time_scaling    = self._parse_scaling(self.time_unit,    "s")
+        self.power_scaling   = self._parse_scaling(self.power_unit,   "W")
+
+        # consume optional debug flags from kwargs
+        self.show_progress = kwargs.pop("show_progress", True)
+        self.show_result   = kwargs.pop("show_result",   True) 
+        self.print_debug   = kwargs.pop("print_debug",   True)
+        self.sanity_limits = kwargs.pop("sanity_limits", DEFAULT_SANITY_CHECK_LIMITS)
+
+        # rest of kwargs passed to animation.save()
+        self.anim_save_kwargs = kwargs
+
+
+    def _parse_scaling(self, unit, base_unit):
+        """Parse unit and return scaling factor to display 'base_unit' quantities in 'unit'.
+        For example, for a prefix with scale 1e-3 (mW, mm, ...), we need to multiply everything with 1e3.
+        """ 
+        if unit == base_unit:
+            return 1
+
+        if  len(unit) != len(base_unit) + 1:
+            raise ValueError(f"unknown unit {unit} (base {base_unit}), expect unit to be SI-prefix + base_unit")
+        
+        try:
+            return 1/SI_prefix_scaling[unit[0]]  
+        except KeyError:
+            raise ValueError(f"unknown unit {unit} - {unit[0]} is not a valid SI prefix")
+
+
+    def load_video_data(self) -> VideoData:
+        with chdir(self.work_folder):
+            video_names = VideoFileNames(self.sim_params.output_filename)
+            video_dat_LRp = np.loadtxt(video_names.filename_LRp, unpack=True, ndmin=2)
+            video_dat_RLp = np.loadtxt(video_names.filename_RLp, unpack=True, ndmin=2)
+            video_dat_car = np.loadtxt(video_names.filename_car, unpack=True, ndmin=2)
+ 
+        return _VideoAnimator.VideoData(video_dat_LRp, video_dat_RLp, video_dat_car)
+    
+    def load_input_output_power(self) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        with chdir(self.work_folder):
+            P_LR_in, F_LR_in, P_RL_in, F_RL_in = np.loadtxt(self.sim_params.signal_input_file, dtype=float, unpack=True, ndmin=2)
+            P_LR_out, F_LR_out, P_RL_out, F_RL_out = np.loadtxt(self.sim_params.output_filename, dtype=float, unpack=True, ndmin=2)
+
+        return P_LR_in, P_LR_out, P_RL_in, P_RL_out
+
+    
+    def check_frame_limits(self, frame_count):
+        if self.sanity_limits:
+            if err_limit := self.sanity_limits.get("error", None):
+                if frame_count > err_limit:
+                    print(f'ERROR: video data would have {frame_count} frames, ABORTING encode') 
+                    return  
+            if warn_limit := self.sanity_limits.get("warn", None):
+                if frame_count > warn_limit:
+                    print(f'WARNING: video data has {frame_count} frames, resulting file will be large and encoding will take a while')
+
+
+    def show_laser_ends(self, ax: plt.Axes):
+        if self.laser_pigtail:
+            x_len = self.laser_pigtail * self.segment_scaling
+            end_x = ax.get_xlim()[1] - x_len
+
+            ax.axvline(x_len, color="gray", linestyle="--")
+            ax.axvline(end_x, color="gray", linestyle="--")
+
+            # update ticks such that the laser starts at 0 and remove labels outside the laser
+            ticks_x = np.arange(x_len, end_x, 250) # TODO make tick spacing a parameter?
+            
+            ax.set_xticks(ticks_x, (ticks_x-x_len).astype(int))
+
+
+    def make_room_for_legend(self, *axes:plt.Axes):
+        # shrink current axis's height by 10% to make some room for the legend at the top
+        for ax in axes:
+            pos = ax.get_position()
+            ax.set_position([pos.x0, pos.y0, pos.width, pos.height * 0.9])
+
+
+    ### main animation function where the magic happens
+    def animate(self):
+        video_data = self.load_video_data()
+        P_LR_in, P_LR_out, P_RL_in, P_RL_out = self.load_input_output_power()
+
+        num_segments, num_frames = np.shape(video_data.LRp)
+
+        max_LRp = video_data.LRp.max()
+        max_RLp = video_data.RLp.max()
+        max_car = video_data.car.max()
+
+        max_y = max(max_LRp, max_RLp)
+
+        frame_slice, frame_slice_count = _to_slice_and_num_frames(self.frame_range, num_frames)  
+
+        if self.print_debug:
+            print('Nr of frames:', frame_slice_count, 'Nr of segments:', num_segments)
+
+        self.check_frame_limits(frame_slice_count)
+
+        # copy data to a 3d array and scale all data to 1
+        npdata = np.zeros((num_segments, 3, num_frames))
+        npdata[:,0,frame_slice] = video_data.LRp[:,frame_slice] / max_y
+        npdata[:,1,frame_slice] = video_data.RLp[:,frame_slice] / max_y
+        npdata[:,2,frame_slice] = video_data.car[:,frame_slice] / max_car
+
+        max_y = 1.0
+        # max_y = video_data.car.max()
+        if self.print_debug:
+            print('Maximum value in video data: ', max_y)
+
+        # First set up the figure, the axis, and the plot element we want to animate
+        fig, (axin, ax, axout) = plt.subplots(1, 3, figsize=(12, 5), tight_layout=True)
+
+        # the center plot has the laser animation
+        max_x = num_segments * self.segment_scaling * self.sim_params.simulation_segment_length() 
+
+        ax.set_xlim(0, max_x)
+        ax.set_ylim(0, max_y)
+
+        self.show_laser_ends(ax)
+        
+        # initialize one "empty" line for LR, RL and carrier
+        lines = []
+        for color in ("blue", "red", "green"):
+            lobj = ax.plot([],[], lw=2, color=color)[0]
+            lines.append(lobj)
+
+        # initialization function: plot the background of each frame
+        def init():    
+            for line in lines:
+                line.set_data([], [])
+            return lines
+
+        x = np.linspace(1, num_segments, num_segments) * self.segment_scaling * self.sim_params.simulation_segment_length() 
+        x_label = f"Length ({self.segment_unit})"
+
+        # the left plot is a moving window input plot
+        axin_x = np.arange(len(P_LR_in)) * self.sim_params.simulation_time_step()
+        axin.plot(axin_x * self.time_scaling, P_LR_in * self.power_scaling, color='blue')
+        axin.set_xlabel(f"Time ({self.time_unit})")
+        axin.set_ylabel(f"Input Power ({self.power_unit})")
+        axin.set_ylim(0) 
+        axin.invert_xaxis()  
+
+        # the right plot is a moving window output plot
+        # we plot the whole output and then show only part of it
+        axout_x = np.arange(self.sim_params.nr_cycles) * self.sim_params.simulation_time_step()
+        axout.plot(axout_x * self.time_scaling, P_LR_out * self.power_scaling, color='blue')
+        axout.set_xlabel(f"Time ({self.time_unit})")
+        axout.set_ylabel(f"Output Power ({self.power_unit})")
+        axout.yaxis.tick_right()
+        axout.yaxis.set_label_position("right")
+        axout.set_ylim(0)
+        # by inverting the axis, the rightmost datapoint of the center graph 
+        # matches the leftmost datapoint of the output graph
+        axout.invert_xaxis() 
+
+        fig.tight_layout()
+        # note that this assumes left and right graphs use same color code
+        self.make_room_for_legend(ax, axin, axout)
+        ax.legend(handles=lines, labels=["Optical Power L->R", "Optical Power R->L", "Carrier density"], 
+                  bbox_to_anchor=(0.5, 1.02), loc='lower center', ncol=3)
+
+        def animate(i):
+            timestamp = self.video_start_t + i * self.frame_delta_t
+            ax.set_xlabel(x_label + f' [time {fmt_eng(timestamp, digits=4)}s]')
+
+            for lnum, line in enumerate(lines):        
+                line.set_data(x, npdata[:,lnum,i])
+
+            time_window = 500e-12 # TODO make this an input parameter
+            axin.set_xlim((timestamp + time_window) * self.time_scaling, timestamp * self.time_scaling) 
+            axout.set_xlim(timestamp * self.time_scaling, (timestamp - time_window) * self.time_scaling) 
+            return lines
+        
+        # show a progress bar if needed - this is recommended as encoding can take a 
+        # couple of second up to minutes for longer animations
+        frames_iter = range(*frame_slice.indices(num_frames))
+        if self.show_progress:
+            # wrap iter with a tqdm to show a progress bar (tqdm is iterable too)
+            frames_iter = tqdm(frames_iter, desc="writing frames", colour="red")
+
+        # call the animator. blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, animate, init_func=init, frames=frames_iter, 
+                                       interval=100, blit=True)
+
+        # save the animation as an mp4.  This requires ffmpeg or mencoder to be
+        # installed.  The extra_args ensure that the x264 codec is used, so that
+        # the video can be embedded in html5.  You may need to adjust this for
+        # your system: for more information, see
+        # http://matplotlib.sourceforge.net/api/animation_api.html
+        # anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+        if self.output_filename[-4:] != '.mp4':
+            print(f"WARNING: output_filename '{self.output_filename}' does not end with .mp4.")
+
+        # save movie to work_folder
+        with chdir(self.work_folder):
+            anim.save(self.output_filename, **self.anim_save_kwargs)
+
+        if self.show_result:
+            plt.show()
+
 
 def animate_video_slice(work_folder, output_filename, 
                         sim_params: PHIsim_SimulationParams,
@@ -64,20 +332,6 @@ def time_window_to_video_frames(time_window: tuple[float, float], sim_params: PH
     return frame_start, frame_end
 
 
-"""
-The function below is based on a script provided by Erwin Bente (e.a.j.m.bente@tue.nl), 
-which was packaged with PHIsim (https://sites.google.com/tue.nl/phisim/home),
-and originally adapted the code from Jake Vanderplas, see below:
-------------------------------
-Matplotlib Animation Example
-
-author: Jake Vanderplas
-email: vanderplas@astro.washington.edu
-website: http://jakevdp.github.com
-license: BSD
-Please feel free to use and modify this, but keep the above information. Thanks!
-http://jakevdp.github.io/blog/2012/08/18/matplotlib-animation-tutorial/
-"""
 def animate_video(work_folder, output_filename, 
                   timestamp_info : tuple[float, float],
                   # optional arguments:
@@ -149,7 +403,7 @@ def animate_video(work_folder, output_filename,
     if max_RLp > max_LRp:
         max_y = max_RLp 
 
-    frame_slice, frame_slice_count = __to_slice_and_num_frames(frame_range, nr_frames)  
+    frame_slice, frame_slice_count = _to_slice_and_num_frames(frame_range, nr_frames)  
 
     if print_debug:
         print('Nr of frames:', frame_slice_count, 'Nr of segments:', nr_sl_data)
@@ -182,12 +436,14 @@ def animate_video(work_folder, output_filename,
     if segment_length:
         max_x *= segment_length[0]
 
-    fig   = plt.figure()
-    ax    = plt.axes(xlim=(0, max_x), ylim=(0, max_y))
+    fig, (ax0, ax1)   = plt.subplots((2, 1))
+    #ax    = plt.axes(xlim=(0, max_x), ylim=(0, max_y))
+    ax0.set_xlim(0, max_x)
+    ax0.set_ylim(0, max_y)
 
     lines = []
     for index,lay in enumerate(plotlays):
-        lobj = ax.plot([],[],lw=2,color=plotcols[index])[0]
+        lobj = ax0.plot([],[],lw=2,color=plotcols[index])[0]
         lines.append(lobj)
 
     # initialization function: plot the background of each frame
@@ -205,7 +461,7 @@ def animate_video(work_folder, output_filename,
 
     def animate(i):
         timestamp = timestamp_info[0] + i * timestamp_info[1]
-        ax.set_xlabel(x_label + f' [time {fmt_eng(timestamp, digits=4)}s]')
+        ax0.set_xlabel(x_label + f' [time {fmt_eng(timestamp, digits=4)}s]')
 
         for lnum,line in enumerate(lines):        
             line.set_data(x, npdata[:,lnum,i])
@@ -241,7 +497,7 @@ def animate_video(work_folder, output_filename,
         plt.show()
 
 
-def __to_slice_and_num_frames(frame_range : FrameRange, nr_frames:int) -> tuple[slice, int]:
+def _to_slice_and_num_frames(frame_range : FrameRange, nr_frames:int) -> tuple[slice, int]:
     if frame_range is not None:
         start, end = frame_range
         if start is not None:
@@ -256,6 +512,15 @@ def __to_slice_and_num_frames(frame_range : FrameRange, nr_frames:int) -> tuple[
 
     return frame_slice, (stop_idx - start_idx)
 
+#################################################################################################
+# The following functions are an attempt to create a still image of a laser animation
+# - wireframe_plot plots the animation in a 3D projection plot
+# - progressive_plot_2d squishes the y-axis of a single frame, so many can be stacked on top of
+#   each other such that the y-axis doubles as time axis.
+# 
+# Both approaches have their downsides and I'm not super happy with the results so far. But I'm 
+# leaving the code just in case it might be useful in the future.
+#################################################################################################
 
 class VideoFileNames:
     def __init__(self, phisimout_name:str):
@@ -301,7 +566,7 @@ def wireframe_plot(work_folder,
     if max_RLp > max_LRp:
         max_y = max_RLp 
 
-    frame_slice, frame_slice_count = __to_slice_and_num_frames(frame_range, nr_frames)  
+    frame_slice, frame_slice_count = _to_slice_and_num_frames(frame_range, nr_frames)  
 
     # build 2D x, y, z arrays for plotting
     z_LRp = video_dat_LRp[:,frame_slice] / max_y
@@ -400,7 +665,7 @@ def progressive_plot_2d(work_folder,
     if max_RLp > max_LRp:
         max_y = max_RLp 
 
-    frame_slice, frame_slice_count = __to_slice_and_num_frames(frame_range, nr_frames)  
+    frame_slice, frame_slice_count = _to_slice_and_num_frames(frame_range, nr_frames)  
 
     # build 2D x, y, z arrays for plotting
     z_LRp = video_dat_LRp[:,frame_slice] / max_y
--- tools/fmt_utils.py
+++ tools/fmt_utils.py
@@ -1,5 +1,46 @@
 from prefixed import Float  # Float with better formatting options. To install, run: pip install prefixed
 
+### thanks to Yatharth Agarwal @ https://stackoverflow.com/a/10970888/4431955
+SI_prefix_scaling = {
+    'y': 1e-24,  # yocto
+    'z': 1e-21,  # zepto
+    'a': 1e-18,  # atto
+    'f': 1e-15,  # femto
+    'p': 1e-12,  # pico
+    'n': 1e-9,   # nano
+    'μ': 1e-6,   # micro (original author put 'u' here, but Python should have no issues with 'μ')
+    'm': 1e-3,   # mili
+    'c': 1e-2,   # centi
+    'd': 1e-1,   # deci
+    'k': 1e3,    # kilo
+    'M': 1e6,    # mega
+    'G': 1e9,    # giga
+    'T': 1e12,   # tera
+    'P': 1e15,   # peta
+    'E': 1e18,   # exa
+    'Z': 1e21,   # zetta
+    'Y': 1e24,   # yotta
+
+    "yocto" : 1e-24,
+    "zepto" : 1e-21,
+    "atto"  : 1e-18,
+    "femto" : 1e-15,
+    "pico"  : 1e-12,
+    "nano"  : 1e-9,
+    "micro" : 1e-6,
+    "mili"  : 1e-3,
+    "centi" : 1e-2,
+    "deci"  : 1e-1,
+    "kilo"  : 1e3, 
+    "mega"  : 1e6, 
+    "giga"  : 1e9, 
+    "tera"  : 1e12,
+    "peta"  : 1e15,
+    "exa"   : 1e18,
+    "zetta" : 1e21,
+    "yotta" : 1e24,
+}
+
 
 def fmt_pow10(val: float, precision: int = 2, times="times") -> str:
     """formatting function for matplotlib labels
@@ -24,6 +65,7 @@ def plot_latex_style(pyplot, font_size=11.0):
     """makes plots looks prettier with LaTeX fonts
     Use this for creating pubishable plots - it's a bit slower than regular style plotting,
     so you probably want to avoid using this for everyday use.
+    Note: this requires a working LaTeX installation! 
     """
     pyplot.rcParams.update({
         "font.size":         font_size,
@@ -35,3 +77,5 @@ def plot_latex_style(pyplot, font_size=11.0):
         "text.latex.preamble": "\\DeclareUnicodeCharacter{03BC}{\\textmu}\n" + # fix for "μ is not set up" error
                                "\\usepackage{amsmath}\\usepackage{amssymb}\\usepackage{siunitx}[=v2]",
     })
+
+
